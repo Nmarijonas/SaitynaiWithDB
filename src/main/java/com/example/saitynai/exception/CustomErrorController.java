4ebf36507eaa7d0155ba2a0dfe3f5816caa992e7
Nmarijonas <Nmarijonas@gmail.com>
2020-12-01 18:14:48 +0200

    update commit

--- src/main/java/com/example/saitynai/exception/CustomErrorController.java
+++ src/main/java/com/example/saitynai/exception/CustomErrorController.java
@@ -1,4 +1,4 @@
-package com.example.saitynai.exception;
+//package com.example.saitynai.exception;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
@@ -8,33 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class CustomErrorController implements ErrorController {
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-
-            if (statusCode == HttpStatus.NOT_FOUND.value() ||
-                    statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-404";
-            } else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
-                return "error-401";
-            } else {
-                return "error-404";
-            }
-            /*
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
-            }*/
-        }
-        return "error";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return null;
-    }
-}
+//@Controller
+//public class CustomErrorController implements ErrorController {
+//    @RequestMapping("/error")
+//    public String handleError(HttpServletRequest request) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+////        if (status != null) {
+////            int statusCode = Integer.parseInt(status.toString());
+////
+////            if (statusCode == HttpStatus.NOT_FOUND.value() ||
+////                    statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+////                return "error-404";
+////            } else if (statusCode == HttpStatus.UNAUTHORIZED.value()) {
+////                return "error-401";
+////            } else {
+////                return "error-404";
+////            }
+//            /*
+//            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                return "error-500";
+//            }*/
+//        }
+//        return "error";
+//    }
+//
+//    @Override
+//    public String getErrorPath() {
+//        return null;
+//    }
+//}
